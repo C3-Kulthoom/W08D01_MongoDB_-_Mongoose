24db218b8682b56981be1b7754c46fe34968f767
G11_Kulthoom <kulthumradi94@gmail.com>
2021-08-23 18:36:53 +0300

    w08d01 solution

--- routers/routes/todos.js
+++ routers/routes/todos.js
@@ -12,6 +12,6 @@ const todoRouter = express.Router();
 todoRouter.get("/todos", getAllTodos);
 todoRouter.post("/create/todos", createTodo);
 todoRouter.put("/update/todos/:task", updateTodo);
-todoRouter.delete("/delete/todos:task", deleteTodo);
+todoRouter.delete("/delete/todos/:task", deleteTodo);
 todoRouter.get("/completed/todos",completedtaskes );
 module.exports = todoRouter;
